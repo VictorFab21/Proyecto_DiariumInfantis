7fee5be77342005b511b5431ea905f8bc24b13af
Víctor Rodríguez <victor.rdzfabela21@gmail.com>
2026-06-03 18:15:55 -0600

    Cambio temp

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,34 @@
 require('dotenv').config();
+
 const app = require('./app');
 const connectMongo = require('./config/mongo');
 const { connectMySQL } = require('./config/mysql');
 const PORT = process.env.PORT || 3000;
 
 async function iniciarServidor() {
+
     try {
         await connectMySQL();
-        await connectMongo();
-        app.listen(PORT, () => {
-            console.log("Servidor ejecutándose en puerto " + PORT);
-        });
+        console.log("MySQL conectado");
     } catch (error) {
-        console.log("Error al iniciar servidor");
-        console.log(error);
+        console.log("MySQL no conectado");
+        console.log(error.message);
     }
+
+
+    try {
+        await connectMongo();
+        console.log("MongoDB conectado");
+    } catch (error) {
+        console.log("MongoDB no conectado");
+        console.log(error.message);
+    }
+
+
+    app.listen(PORT, () => {
+        console.log("Servidor ejecutándose en puerto " + PORT);
+    });
+
 }
+
 iniciarServidor();
